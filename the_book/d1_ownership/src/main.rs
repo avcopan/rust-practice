248af91890def04e9aae4bee39bb12b9fe2a9ec5
Andreas Copan <avcopan@gmail.com>
2024-06-04 09:36:02 -0400

    Section 4.1 (incomplete)

---
+++ the_book/d1_ownership/src/main.rs
@@ -0,0 +1,22 @@
+// Rust ownership rules:
+//  1. Each value has an *owner*
+//  2. There can only be one owner at a time
+//  3. When the owner goes out of scope, the value is dropped
+fn main() {
+    // Variable Scope
+    {
+        let s = "hello"; // `s` comes into scope
+        // here, we are using a string literal, which are values hardcoded into
+        // the program; unlike `String` values, string literals can be stored on
+        // the stack; string literals are *always* immutable!
+        println!("{s} there"); // `s` is in scope, so you can do stuff with it
+    } // `s` goes out of scope
+
+    // The `String` type
+    //  - unlike str literals, Strings can be made mutable with the mut keyword
+    //  - allowing them to be mutable means they must be stored on the *heap*,
+    //  because the necessary memory allocation is not known at compile time
+    let mut s = String::from("hello");
+    s.push_str(", world!"); // appends a literal to the `String`
+    println!("{}", s);
+}
