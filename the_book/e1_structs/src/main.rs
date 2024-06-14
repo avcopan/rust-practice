7bc8ffc9977d013ed706a1e1bbc3f4aa1e2b4202
Andreas Copan <avcopan@gmail.com>
2024-06-14 09:28:38 -0400

    Section 5.1

--- the_book/e1_structs/src/main.rs
+++ the_book/e1_structs/src/main.rs
@@ -6,6 +6,15 @@ struct User {
     sign_in_count: u64,
 }
 
+#[derive(Debug)] // allows the struct to be printed
+struct Color(i32, i32, i32);
+#[derive(Debug)] // allows the struct to be printed
+struct Point(i32, i32, i32);
+
+#[derive(Debug)] // allows the struct to be printed
+struct DatalessType;
+
+
 fn new_user(email: String, username: String) -> User {
     User {
         active: true,
@@ -27,11 +36,52 @@ fn main() {
     println!("{:?}", user1);
 
     let mut user2 = new_user(
-        String::from("jimshmilligan@gmail.com"), 
+        String::from("jimshmilligan@gmail.com"),
         String::from("jimshmilligan")
     );
     user2.active = false;
     user2.email = String::from("jimshmilligan@yahoo.com");
     user2.sign_in_count += 1;
     println!("{:?}", user2);
+
+    // Creating Instances from Other Instances with Struct Update Syntax
+    //  - instantiating with `..<other instance>` fills in missing values using
+    //  another instance
+    let user3 = User {
+        email: String::from("bobhyffligan@yahoo.com"),
+        ..user1
+    };
+    println!("{:?}", user3);
+    //  - NOTE: This **moves** any heap variables from `user1` over to `user3`
+    //  - In this case, user1.username is moved to user3.username
+    // println!("{:?}", user1);
+    //  ^ error: borrow of partially moved value: `user1`
+
+    //  - The stack variables get copied, so, if we replace all of the heap
+    //  variables, then the original instance that we update from remains valid
+    let user4 = User {
+        username: String::from("jimshmil"),
+        email: String::from("jimshmil@hotmail.com"),
+        ..user2
+    };
+    println!("{:?}", user4);
+    println!("{:?}", user2);
+
+    // Tuple Structs
+    let black = Color(0, 0, 0);
+    let origin = Point(0, 0, 0);
+    let point2 = Point(-5, 8, -9);
+    println!("{}", black.0);
+    println!("{}", black.1);
+    println!("{}", black.2);
+    println!("{}", point2.0);
+    println!("{}", point2.1);
+    println!("{}", point2.2);
+    println!("{:?}", black);
+    println!("{:?}", origin);
+    println!("{:?}", point2);
+
+    // Unit-Like Structs Without Any Fields
+    let d = DatalessType;
+    println!("{:?}", d);
 }
