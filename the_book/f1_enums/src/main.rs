0330aed9660152a13a94215ece24e2cc64a99d04
Andreas Copan <avcopan@gmail.com>
2024-06-19 09:44:55 -0400

    Section 6.1 (incomplete)

--- the_book/f1_enums/src/main.rs
+++ the_book/f1_enums/src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+#[derive(Debug)]
+enum IpAddress {
+    V4(u8, u8, u8, u8),
+    V6(String),
 }
+
+fn main() {
+    let home = IpAddress::V4(127, 0, 0, 1);
+    let loopback = IpAddress::V6(String::from("::1"));
+
+    println!("{:#?}", home);
+    println!("{:#?}", loopback);
+}
+
+// fn route(ip_type: IpAddressType) {}
