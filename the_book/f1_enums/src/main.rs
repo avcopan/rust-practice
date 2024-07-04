3df8084a3d7c6b67b2a5973cb970475ce3bcb2ef
Andreas Copan <avcopan@gmail.com>
2024-07-04 09:48:23 -0400

    Section 6.1
    Need to revisit the IP address example, because I don't understand how
    to access the values in the enum.

--- the_book/f1_enums/src/main.rs
+++ the_book/f1_enums/src/main.rs
@@ -1,15 +1,27 @@
-#[derive(Debug)]
-enum IpAddress {
-    V4(u8, u8, u8, u8),
-    V6(String),
-}
+// #[derive(Debug)]
+// enum IpAddress {
+//     V4(u8, u8, u8, u8),
+//     V6(String),
+// }
 
 fn main() {
-    let home = IpAddress::V4(127, 0, 0, 1);
-    let loopback = IpAddress::V6(String::from("::1"));
+    // let home = IpAddress::V4(127, 0, 0, 1);
+    // let loopback = IpAddress::V6(String::from("::1"));
 
-    println!("{:#?}", home);
-    println!("{:#?}", loopback);
+    // println!("{:#?}", home);
+    // println!("{:#?}", loopback);
+
+    // The Option enum
+    //  - The Option enum prevents us from using Option<T> as if it were a T
+    //  value; It forces us to deal with the possibility of an absent value
+    //  - This also means that when we have T, we can proceed confidently
+    //  knowing that the value is valid -- if not, the compiler will tell us
+    let some_char = Some('e');
+    let some_number = Some(5);
+    // let sum = some_number + 5;
+    // ^ error: cannot add `{integer}` to `Option<{integer}>`
+    let absent_number: Option<i32> = None;
+    println!("{:?}", some_char);
+    println!("{:?}", some_number);
+    println!("{:?}", absent_number);
 }
-
-// fn route(ip_type: IpAddressType) {}
